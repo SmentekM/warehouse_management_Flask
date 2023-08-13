6d6c3a2bab9e97298ae1bac1e1988ebe8aede54d
Marcin <marcin.smentek@gamil.com>
2023-08-13 14:33:05 +0200

    dodano kod do zakupu, kod do zakresu historii

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,url_for, redirect
 from klasa_manager import *
 
 app = Flask(__name__)
@@ -10,35 +10,58 @@ def index():
     title = "Witaj na stronie głownej Twojej firmy"
     manager.execute('wczytanie_plikow')
 
-    # nazwa = str(request.form.get("nazwa"))
-    # ilosc = float(request.form.get("ilosc",0))
+    nazwa = str(request.form.get("nazwa"))
+    ilosc_do_sprzedazy = request.form.get("ilosc")
     operacja = str(request.form.get("rodzaj"))
     kwota = request.form.get("kwota")
+    nazwa_produktu = request.form.get('nazwa_zakupu')
+    ilosc_do_zakupu = request.form.get('ilosc_zakupiona')
+    cena = request.form.get('cena')
     if operacja and kwota:
         kwota=float(kwota)
-        saldo(manager,operacja=operacja,kwota = kwota)
+        saldo(manager, operacja=operacja, kwota=kwota)
+
+    if nazwa and ilosc_do_sprzedazy:
+        ilosc_do_sprzedazy = float(ilosc_do_sprzedazy)
+        sprzedaz(manager,produkt_do_przedazy=nazwa, ilosc_do_sprzedazy=ilosc_do_sprzedazy)
+    if nazwa_produktu and ilosc_do_zakupu and cena:
+        ilosc_do_zakupu = float(ilosc_do_zakupu)
+        cena = float(cena)
+        zakup(manager, produkt=nazwa_produktu, cena_zakupu=cena, ilosc=ilosc_do_zakupu)
     manager.execute('zapisz_w_pliku')
-    # manager.execute('saldo',1000)
-
-
 
     context = {
         "title": title,
         "saldo": manager.konto,
         "magazyn": manager.magazyn,
-        # "sprzedaż": sprzedaz(konto, produkt_do_przedazy= nazwa,ilosc_do_sprzedazy=ilosc),
-        # "konto": saldo( operacja=operacja, kwota=kwota),
-
+      
     }
     return render_template('strona glowna.html', context=context)
 
-@app.route('/history')
+@app.route('/history',methods=['POST', 'GET'])
 def history():
-    title = "Historia operacji"
+    if request.method == "POST":
+        start = request.form.get("start")
+        koniec = request.form.get("koniec")
+        return redirect(url_for("zakres_histori",start=start, koniec=koniec))
+    else:
+
+        title = "Historia operacji"
+        context = {
+            "title": title,
+             "historia": manager.historia
+        }
+        return render_template('history.html', context=context)
+@app.route('/history/<start>/<koniec>')
+def zakres_histori(start, koniec):
+    title = "Historia operacj w wybranym zakresie"
+
+
     context = {
         "title": title,
-         "historia": manager.historia
+        "historia": przeglad(manager, od=start, do=koniec)}
+#
+#
+    return render_template('history_zakres.html', context=context)
 
-    }
-    return render_template('history.html', context=context)
 
