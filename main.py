c9f48381c75d17ee40ab027adf20fdff8879af44
Marcin <marcin.smentek@gamil.com>
2023-08-13 16:44:47 +0200

    przeniesiona wczytywanie plikow ponad funckje

--- main.py
+++ main.py
@@ -4,31 +4,35 @@ from klasa_manager import *
 app = Flask(__name__)
 
 
-
+manager.execute('wczytanie_plikow')
 @app.route('/', methods=['POST', 'GET'])
 def index():
     title = "Witaj na stronie głownej Twojej firmy"
-    manager.execute('wczytanie_plikow')
 
-    nazwa = str(request.form.get("nazwa"))
-    ilosc_do_sprzedazy = request.form.get("ilosc")
-    operacja = str(request.form.get("rodzaj"))
-    kwota = request.form.get("kwota")
-    nazwa_produktu = request.form.get('nazwa_zakupu')
-    ilosc_do_zakupu = request.form.get('ilosc_zakupiona')
-    cena = request.form.get('cena')
-    if operacja and kwota:
-        kwota=float(kwota)
-        saldo(manager, operacja=operacja, kwota=kwota)
+    if request.method == "POST":
+        nazwa = str(request.form.get("nazwa"))
+        ilosc_do_sprzedazy = request.form.get("ilosc")
+        operacja = str(request.form.get("rodzaj"))
+        kwota = request.form.get("kwota")
+        nazwa_produktu = request.form.get('nazwa_zakupu')
+        ilosc_do_zakupu = request.form.get('ilosc_zakupiona')
+        cena = request.form.get('cena')
+        if operacja and kwota:
+            kwota=float(kwota)
+            saldo(manager, operacja=operacja, kwota=kwota)
+            manager.execute('zapisz_w_pliku')
 
-    if nazwa and ilosc_do_sprzedazy:
-        ilosc_do_sprzedazy = float(ilosc_do_sprzedazy)
-        sprzedaz(manager,produkt_do_przedazy=nazwa, ilosc_do_sprzedazy=ilosc_do_sprzedazy)
-    if nazwa_produktu and ilosc_do_zakupu and cena:
-        ilosc_do_zakupu = float(ilosc_do_zakupu)
-        cena = float(cena)
-        zakup(manager, produkt=nazwa_produktu, cena_zakupu=cena, ilosc=ilosc_do_zakupu)
-    manager.execute('zapisz_w_pliku')
+        if nazwa and ilosc_do_sprzedazy:
+            ilosc_do_sprzedazy = float(ilosc_do_sprzedazy)
+            sprzedaz(manager, produkt_do_przedazy=nazwa, ilosc_do_sprzedazy=ilosc_do_sprzedazy)
+            manager.execute('zapisz_w_pliku')
+
+        if nazwa_produktu and ilosc_do_zakupu and cena:
+            ilosc_do_zakupu = int(ilosc_do_zakupu)
+            cena = float(cena)
+            zakup(manager, produkt=nazwa_produktu, cena_zakupu=cena, ilosc=ilosc_do_zakupu)
+            print(manager.historia)
+            manager.execute('zapisz_w_pliku')
 
     context = {
         "title": title,
@@ -40,6 +44,7 @@ def index():
 
 @app.route('/history',methods=['POST', 'GET'])
 def history():
+
     if request.method == "POST":
         start = request.form.get("start")
         koniec = request.form.get("koniec")
@@ -52,16 +57,15 @@ def history():
              "historia": manager.historia
         }
         return render_template('history.html', context=context)
+
 @app.route('/history/<start>/<koniec>')
 def zakres_histori(start, koniec):
     title = "Historia operacj w wybranym zakresie"
 
-
     context = {
         "title": title,
         "historia": przeglad(manager, od=start, do=koniec)}
-#
-#
+
     return render_template('history_zakres.html', context=context)
 
 
